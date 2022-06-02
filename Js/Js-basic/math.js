48c4dfb1163461cabb8b67e617c4f2b1ab02a3e6
shyunte <sht4441@gmail.com>
2022-06-02 22:42:33 +0900

    Math method

---
+++ Js/Js-basic/math.js
@@ -0,0 +1,30 @@
+
+/*
+        Math.method()
+
+Math.floor () : 내림
+Math.ceil ()  : 올림
+Math.round()  : 반올림
+
+.toFixed(2) : 소수 둘째자리까지 표기
+.toFixed(0) : 소수 표기 x 
+.toFixed(5) :  값의 소수점길이보다 길때는 ex) 1.234 이면 1.23400으로 0으로 채움
+
+
+.isNAN()은  NAN이 맞는지 아닌지 구분해주는것 
+만약 isNAN(x)는 NaN이기 때문에 true
+만약 isNAN(5)는 NaN이 아니기 때문에 false
+
+parseInt()는 두번재 파라미터를 받아서 진수를 정할수 있다.
+parseInt(10, 2) : 2 진수로 표기 
+parseInt(22, 16) : 16진수로 표기 
+
+parseFloat(shyun) : 19.5  부동소수반환         >          let shyun = '19.5%';   
+pareInt()는 소수점 버림     
+
+Math.abs() : 절대값
+Math.pow(n,m): 제곱  Math.pow(2,3) = 8
+Math.sqrt() : 제곱근 Math.sqrt(16) = 4 
+
+
+*/
