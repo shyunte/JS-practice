9eb449cfe9626f9cc415345ac7fdd2f20dc7f1b2
shyun <96578042+shyunte@users.noreply.github.com>
2022-06-08 22:43:33 +0900

    fix spread syntax

--- Js/Js-basic/cleancoding.js
+++ Js/Js-basic/cleancoding.js
@@ -65,3 +65,31 @@
 
 
 
+{
+  //각 배열에서 짝수만 뽑아 그 값에 *4를 하고 그 값들을 모두 더해라!
+  const item = [1, 2, 3, 4, 5, 6, 7, 8, 8, 9, 10];
+  function getAll(items) {
+    return items.filter((num) => num % 2 === 0);
+  }
+  function multiple(items) {
+    return items.map((num) => num * 4);
+  }
+  function sumArray(items) {
+    items.reduce((a, b) => a + b, 0);
+  }
+
+  // function 없이 사용도 가능 더 욱 깔끔함
+  const result = items
+    .filter((num) => num % 2 === 0)
+    .map((num) => num * 4)
+    .reduce((a, b) => a + b, 0);
+}
+
+{
+  //중복 값 제거하기 Array
+
+  const array = ["🍐", "🍌", "🍇", "🍒", "🍐", "🍏"];
+  console.log(array);
+
+  console.log([...new Set(array)]);
+}
