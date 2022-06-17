df540f130ea6beb6df9d9a1a1c2655ed2d32de43
shyun <96578042+shyunte@users.noreply.github.com>
2022-06-17 19:46:59 +0900

    rest API

--- Js/node/buffer/buffer-memory.js
+++ Js/node/buffer/buffer-memory.js
@@ -1,6 +1,6 @@
-const fs = require('fs');
-console.log('before:', process.memoryUsage().rss);  //rss로 메모리를 체크함
+const fs = require("fs");
+console.log("before:", process.memoryUsage().rss); //rss로 메모리를 체크함
 
-const data1 = fs.readFileSync('./big.txt');
-fs.writeFileSync('./big.txt', data1);
-console.log('buffer', process.memoryUsage().rss); //
+const data1 = fs.readFileSync("./big.txt");
+fs.writeFileSync("./big.txt", data1);
+console.log("buffer", process.memoryUsage().rss); //
--- Js/node/server/server2.js
+++ Js/node/server/server2.js
@@ -1,22 +1,24 @@
-const http = require('http');
-const fs =require('fs').promises;
+const http = require("http");
+const fs = require("fs").promises;
 
-const server = http.createServer( async (req,res) => {  //async 항상 에러처리
-    try{
-    res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
-    const data = await fs.readFile('./server2.html');
-    res.end(data);
-    } catch (error){
-        console.err(err);
-        res.writeHead(200, { 'Content-Type': 'text/plain; charset=utf-8' });
-        res.end(err.message);
+const server = http
+  .createServer(async (req, res) => {
+    //async 항상 에러처리
+    try {
+      res.writeHead(200, { "Content-Type": "text/html; charset=utf-8" });
+      const data = await fs.readFile("./server2.html");
+      res.end(data);
+    } catch (error) {
+      console.error(error);
+      res.writeHead(200, { "Content-Type": "text/plain; charset=utf-8" });
+      res.end(err.message);
     }
-})
+  })
   .listen(8080);
-  server.on('listening', ( ) => {
-    console.log('8080번 포트에서 서버 대기중 입니다.');
-  });
+server.on("listening", () => {
+  console.log("8080번 포트에서 서버 대기중 입니다.");
+});
 
-  server.on('error', (err) => {
-    console.error(err);
-  });
+server.on("error", (err) => {
+  console.error(err);
+});
